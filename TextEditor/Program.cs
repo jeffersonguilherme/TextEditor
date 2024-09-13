0e659c04e13b8529f7ed45626602eca85a063408
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2024-09-13 00:41:08 -0300

    Curso C# mão da massa editor de texto
    menu criado, e funções para abrir arquivo e editar estanciadas

---
+++ TextEditor/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace TextEditor {
+    class Program {
+        static void Main(string[] args){
+            Menu();
+        }
+
+        static void Menu(){
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer");
+            Console.WriteLine("1 - Abrir um arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("0 - Sair");
+            short option = short.Parse(Console.ReadLine());
+
+            switch(option){
+
+                case 0: Environment.Exit(0); break;
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+                default: Menu(); break;
+                
+            }
+        }
+
+        static void Abrir(){
+
+        }
+
+        static void Editar(){
+
+        }
+
+
+    }
+}
