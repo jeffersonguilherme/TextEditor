a1f69924c8039623ddce1b032a1dd0359febbbee
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2024-09-14 11:35:21 -0300

    Curso C# Editor de texto
    Criando função para abrir o aquivo

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -23,7 +23,18 @@
         }
 
         static void Abrir(){
+            Console.Clear();
+            Console.WriteLine("Qual caminho do arquivo?");
+            string path = Console.ReadLine();
 
+            using(var file = new StreamReader(path)){
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
         }
 
         static void Editar(){
