baaedb2c25d68b52aa106fd0cf52257254886df2
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2024-09-13 23:22:26 -0300

    Curso C# Editor de texto
    Função de criação e salvado arquivo com .net

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -27,7 +27,34 @@
         }
 
         static void Editar(){
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
+            Console.WriteLine("-----------------");
+            string text = "";
 
+
+            do{
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+                Salvar(text);
+            
+        }
+
+        static void Salvar(string text){
+            Console.Clear();
+            Console.WriteLine("Qual caminho para salvar o arquivo?");
+            var path = Console.ReadLine();
+
+            using(var file = new StreamWriter(path)){
+                file.Write(text);
+            }
+
+            Console.WriteLine($"Arquivo {path} salvo com sucesso!");
+            Console.ReadLine();
+            Menu();
         }
 
 
